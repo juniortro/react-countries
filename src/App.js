97a17a9244dfe836816ea1322add5c97a18514e2
Carlos Junior <junior_tro@yahoo.com.br>
2021-03-17 13:26:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useContext } from 'react';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header/Header';
+import CountryGridPage from './pages/CountryGridPage/CountryGridPage';
+import CountryDetailsPage from './pages/CountryDetailsPage/CountryDetailsPage.js';
+
+import { AppContext } from './App.provider.js';
+import { getCountries } from './utils';
+
+import './App.scss';
+
+const App = () => {
+    const { theme, setCountries, setAllCountries } = useContext(AppContext);
+
+    useEffect(() => {
+        getCountries().then(({ data }) => {
+            setAllCountries(data);
+            setCountries(data);
+        });
+    }, [setCountries, setAllCountries]);
+
+    return (
+        <div className={`App ${theme === 'dark' ? 'dark' : ''}`}>
+            <Header />
+            <Route exact path="/" component={CountryGridPage} />
+            <Route exact path="/:countryCode" component={CountryDetailsPage} />
+        </div>
+    );
+};
 
 export default App;
